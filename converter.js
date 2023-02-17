085f2d502a595598f4bf389569f70831b13f174f
Raghad Al-Jabr <40861751+raghd-do@users.noreply.github.com>
2023-02-17 21:26:16 +0300

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,45 +1,27 @@
-var manahej = {
-  1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-  3: [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30,
-  ],
-};
+const convert_to = (prev_manhaj, prev_level) => {
+  var manhaj_list = [];
 
-const what_could_be_converted_to = (prev_manhaj, prev_level) => {
-  if (prev_manhaj == 4) {
-    return console.log(
-      `${prev_manhaj} - ${prev_level} can be converted to: 4 - ${
-        prev_level + 1
-      } or 6 - ${prev_level * 2 + 1}`
-    );
-  } else if (prev_manhaj == 6) {
-    if (prev_level % 2 != 0) {
-      return console.log(
-        `${prev_manhaj} - ${prev_level} can't be converted.. try on next level`
-      );
+  if (prev_manhaj === 4) {
+    manhaj_list.push({ m: 4, l: prev_level + 1 });
+    manhaj_list.push({ m: 6, l: prev_level * 2 + 1 });
+    return manhaj_list;
+  } else if (prev_manhaj === 6) {
+    if (prev_level % 2 !== 0) {
+      manhaj_list.push({ m: 6, l: prev_level + 1 });
+      return manhaj_list;
     }
-    return console.log(
-      `${prev_manhaj} - ${prev_level} can be converted to: 4 - ${
-        prev_level / 2 + 1
-      } or 6 - ${prev_level + 1}`
-    );
+    manhaj_list.push({ m: 4, l: prev_level / 2 + 1 });
+    manhaj_list.push({ m: 6, l: prev_level + 1 });
+    return manhaj_list;
   }
 
-  if (prev_level % prev_manhaj != 0) {
-    return console.log(
-      `${prev_manhaj} - ${prev_level} can't be converted.. try on next level`
-    );
+  if (prev_level % prev_manhaj !== 0) {
+    manhaj_list.push({ m: prev_manhaj, l: prev_level + 1 });
+    return manhaj_list;
   }
 
-  return console.log(
-    `${prev_manhaj} - ${prev_level} can be converted to: '1 - ${
-      (prev_level / prev_manhaj) * 1 + 1
-    } or 2 - ${(prev_level / prev_manhaj) * 2 + 1} or 3 - ${
-      (prev_level / prev_manhaj) * 3 + 1
-    }`
-  );
+  manhaj_list.push({ m: 1, l: (prev_level / prev_manhaj) * 1 + 1 });
+  manhaj_list.push({ m: 2, l: (prev_level / prev_manhaj) * 2 + 1 });
+  manhaj_list.push({ m: 3, l: (prev_level / prev_manhaj) * 3 + 1 });
+  return manhaj_list;
 };
-
-what_could_be_converted_to(2, 4);
